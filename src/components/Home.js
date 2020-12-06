773dde71e97f12b0ecce83856f2e9c25157e191a
Peshlo <peshlotechnologies@gmail.com>
2020-12-06 20:49:24 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -61,7 +61,7 @@ class Home extends Component {
                                         
                                     </div>
                                     :
-                                    <div className="receiverDiv"><img src="./Images/videocall-blur.jpg"/>
+                                    <div className="receiverDiv"><img src="src/Images/videocall-blur.jpg"/>
                                         <div className="innerDiv">
                                             <Button className="connectbtn btncoonectbefore" variant="outline-light" onClick={this.togglePopup.bind(this)}>Connect</Button>
                                         </div>
